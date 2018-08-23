3755e33ae24cf22e7857b1ba74895bf0d21eddc7
Bruce Hernandez <hernandez.bruce8@uabc.edu.mx>
2018-08-23 10:47:50 -0700

    Update TODOs

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
      | TODO
      |--------------------------------------------------------------------------------------------------
      | 1. Add more cards ✔
-     | 2. Add a "New Game" button, that will restart the game
+     | 2. Add a "New Game" button, that will restart the game ✔
      | 3. Shuffle the cards in the in Concentration's init()
      | 4. Add themes to the game. At least 6 themes. One must be chosen at random on startup.
      | 5. Make adding a theme as simple as adding a single line of code
